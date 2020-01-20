6c48d4def9b96227ff330d541b9155394a1bd578
Jakub Antczak <jakubantczak.info@gmail.com>
2020-01-20 15:53:47 +0100

    initial commit

---
+++ controllers/feed.js
@@ -0,0 +1,16 @@
+exports.getPost = (req, res, next) => {
+  res.status(200).json({
+    posts: [{ title: "first Jakubs post", content: "this is amazingpost !" }]
+  });
+};
+
+//In API the errors mesages are very important to get know what's happening behind the scenes
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  //create post in db
+  res.status(201).json({
+    message: "Post created succesfully",
+    post: { id: new Date().toISOString(), title: title, content: content }
+  });
+};
